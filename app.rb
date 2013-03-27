29fadd99efa808ca9b9187d52eda1eec453520eb
Zen Savona <z@zensavona.com>
2013-03-27 12:29:08 +1000

    wrap things up

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require 'rubygems' 
 require 'sinatra' 
 
-get '/' do
-  File.new('public/index.html').readlines # mask the extension with a static route
+get '/meta' do
+  File.new('public/meta.html').readlines # mask the extension with a static route
+  # redirect '/index.html' # rediect to the html file
+end
+get '/non-meta' do
+  File.new('public/non-meta.html').readlines # mask the extension with a static route
   # redirect '/index.html' # rediect to the html file
 end
