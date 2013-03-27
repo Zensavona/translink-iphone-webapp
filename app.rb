a115bbed5b4accfff4e6b4aa2bd26996998e1926
Zen Savona <z@zensavona.com>
2013-03-27 12:32:57 +1000

    add meta etc

--- app.rb
+++ app.rb
@@ -5,7 +5,8 @@ get '/meta' do
   File.new('public/meta.html').readlines # mask the extension with a static route
   # redirect '/index.html' # rediect to the html file
 end
-get '/non-meta' do
-  File.new('public/non-meta.html').readlines # mask the extension with a static route
+
+get '/nometa' do
+  File.new('public/nometa.html').readlines # mask the extension with a static route
   # redirect '/index.html' # rediect to the html file
 end
